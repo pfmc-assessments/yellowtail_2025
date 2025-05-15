555f5cec3346f57e8117ef98961c3f067edcfad1
iantaylor-NOAA <ian.taylor@noaa.gov>
2025-05-15 16:00:43 -0700

    make historical assessment timeseries depend on current base model

--- Rscripts/make_r4ss_figs_tables.R
+++ Rscripts/make_r4ss_figs_tables.R
@@ -1,6 +1,6 @@
 # code to set base model directory and update r4ss plots and tables as needed
 
-# location of base model (TODO: change as needed)
+# location of base model 
 # path is relative to /report/
 # base_mod should match what's set in /report/SAR_USWC_Yellowtail_rockfish_skeleton.qmd
 # so is not set here to avoid accidental mismatch
@@ -11,7 +11,7 @@ if (!exists("base_mod")) {
 }
 
 # read model output using r4ss
-model <- r4ss::SS_output(
+mod_out <- r4ss::SS_output(
   dir = file.path("Model_Runs", base_mod),
   SpawnOutputLabel = "Spawning output (trillions of eggs)",
   printstats = FALSE,
@@ -20,17 +20,17 @@ model <- r4ss::SS_output(
 
 # make new r4ss plots
 # TODO: add better fleetnames if desired
-r4ss::SS_plots(model, printfolder = "../../report/r4ss_plots", uncertainty = TRUE)
+r4ss::SS_plots(mod_out, printfolder = "../../report/r4ss_plots", uncertainty = TRUE)
 
 # make new tables
 # TODO: add better fleetnames if desired
-r4ss::table_all(replist = model, dir = here::here("report"))
+r4ss::table_all(replist = mod_out, dir = here::here("report"))
 
 # custom calls to r4ss functions
 
 # taller biology plot to be easier to see and 
 r4ss::SSplotBiology(
-  model,
+  mod_out,
   subplots = 3,
   plotdir = "report/r4ss_plots",
   plot = FALSE,
@@ -41,9 +41,9 @@ r4ss::SSplotBiology(
 # standard selectivity plots but with smurfs removed because 
 # the use of index units 33 bypasses selectivity
 r4ss::SSplotSelex(
-  model,
+  mod_out,
   subplots = c(1, 2),
-  fleets = which(!grepl("SMURF", model$FleetNames)),
+  fleets = which(!grepl("SMURF", mod_out$FleetNames)),
   plotdir = "report/Figures",
   plot = FALSE,
   print = TRUE
@@ -51,12 +51,12 @@ r4ss::SSplotSelex(
 
 # make custom selectivity plot
 source("Rscripts/plot_selex.R")
-plot_yellowtail_tv_selex(model)
+plot_yellowtail_tv_selex(mod_out)
 
 # make custom index plot
 source("Rscripts/plot_indices.R")
 plot_indices(
-  model,
+  mod_out,
   dir = "report/Figures",
   fit = TRUE,
   log = FALSE,
@@ -65,7 +65,7 @@ plot_indices(
 
 # make custom parameter prior/est plot TODO: modify to exclude male M?
 r4ss::SSplotPars(
-  model,
+  mod_out,
   strings = "NatM_uniform_Fem_GP_1",
   ncols = 1,
   nrows = 1,
@@ -81,7 +81,7 @@ r4ss::SSplotPars(
 
 # landscape plot of length comps for fishing fleets
 r4ss::SSplotComps(
-  model,
+  mod_out,
   subplots = 21,
   fleets = 1:3,
   maxrows = 1,
@@ -100,7 +100,7 @@ file.rename(
 )
 
 r4ss::SSplotComps(
-  model,
+  mod_out,
   subplots = 21,
   fleets = 4:6,
   maxrows = 1,
@@ -119,7 +119,7 @@ file.rename(
 )
 
 r4ss::SSplotComps(
-  model,
+  mod_out,
   kind = "AGE",
   subplots = 21,
   fleets = 1:3,
@@ -137,3 +137,6 @@ file.rename(
   "report/Figures/comp_agedat__aggregated_across_time.png",
   "report/Figures/comp_agedat__aggregated_across_time_FISHERIES.png"
 )
+
+# historical assessment timeseries plot
+source("Rscripts/historical_assessment_timeseries.R")
