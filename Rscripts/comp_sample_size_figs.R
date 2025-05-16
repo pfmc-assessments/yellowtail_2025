51c01ef15dc998ed2f2fdfd4bff7a0eef69667dd
iantaylor-NOAA <ian.taylor@noaa.gov>
2025-05-16 12:14:27 -0700

    fix colors, missing ages in comp sample size plots

--- Rscripts/comp_sample_size_figs.R
+++ Rscripts/comp_sample_size_figs.R
@@ -1,3 +1,7 @@
+library(ggplot2)
+theme_set(theme_classic(base_size = 14))
+
+
 # bds_clean is from Rscripts/commercial_comps.R
 bds_clean |>
   group_by(year) |>
@@ -10,7 +14,7 @@ bds_clean |>
   tidyr::pivot_longer(-year, names_to = "Sample type", values_to = "Count") |>
   ggplot() +
   geom_col(aes(x = year, y = Count, fill = factor(`Sample type`, levels = c("Length only", "Age and length")))) +
-  viridis::scale_fill_viridis(discrete = TRUE) +
+  #viridis::scale_fill_viridis(discrete = TRUE) +
   # ggtitle("Commercial Biological Samples by Year") +
   labs(fill = "Sample type") +
   theme(legend.position = "top")
@@ -30,7 +34,7 @@ ashop_comps |>
   tidyr::pivot_longer(-year, names_to = "Sample type", values_to = "Count") |>
   ggplot() +
   geom_col(aes(x = year, y = Count, fill = factor(`Sample type`, levels = c("Length only", "Age and length")))) +
-  viridis::scale_fill_viridis(discrete = TRUE) +
+  #viridis::scale_fill_viridis(discrete = TRUE) +
   # ggtitle("At-Sea Hake Biological Samples by Year") +
   labs(fill = "Sample type") +
   theme(legend.position = "top")
@@ -77,7 +81,7 @@ wcgbts_bio_table |>
   tidyr::pivot_longer(-year, names_to = "Sample type", values_to = "Count") |>
   ggplot() +
   geom_col(aes(x = year, y = Count, fill = factor(`Sample type`, levels = c("Length only", "Age and length")))) +
-  viridis::scale_fill_viridis(discrete = TRUE) +
+  #viridis::scale_fill_viridis(discrete = TRUE) +
   # ggtitle("WCGBTS Biological Samples by Year") +
   labs(fill = "Sample type") +
   theme(legend.position = "top")
@@ -87,6 +91,12 @@ ggsave(
 )
 
 tri_bio_table <- read.csv("Data/Processed/input_n_tri.csv", check.names = FALSE)
+
+# Add all years from 1980 to 2004 (fill missing with zeros)
+all_years <- data.frame(year = 1980:2004)
+tri_bio_table <- dplyr::right_join(tri_bio_table, all_years, by = "year") |>
+  dplyr::arrange(year)
+
 tri_bio_table |>
   dplyr::mutate(dplyr::across(dplyr::everything(), ~ tidyr::replace_na(., 0))) |>
   dplyr::select(year, n_ages, n_lengths) |>
@@ -96,7 +106,7 @@ tri_bio_table |>
   tidyr::pivot_longer(-year, names_to = "Sample type", values_to = "Count") |>
   ggplot() +
   geom_col(aes(x = year, y = Count, fill = factor(`Sample type`, levels = c("Length only", "Age and length")))) +
-  viridis::scale_fill_viridis(discrete = TRUE) +
+  #viridis::scale_fill_viridis(discrete = TRUE) +
   # ggtitle("WCGBTS Biological Samples by Year") +
   labs(fill = "Sample type") +
   theme(legend.position = "top")
@@ -104,3 +114,74 @@ ggsave(
   filename = "report/figures/comp_samples_tri.png",
   width = 6.5, height = 3.5
 )
+
+# hook and line samples from model input files
+# (assuming that Nsamp is the number of fish)
+mod_in$dat$lencomp |>
+  dplyr::filter(fleet == 4) |>
+  dplyr::select(year, Nsamp) |>
+  dplyr::rename(Count = Nsamp) |>
+  ggplot() +
+  geom_col(aes(x = year, y = Count), fill = "#F8766D")
+ggsave(
+  filename = "report/figures/comp_samples_HnL.png",
+  width = 6.5, height = 3.5
+)
+
+# mean length plots
+mean_length_multifleet <- function(fleets = 1:3, type = "len", filename, datonly = TRUE) {
+  # open PNG file
+  png(
+    filename = file.path("report/figures/", filename),
+    width = 6.5, height = 3.5, units = "in", res = 300
+  )
+  ylim <- c(15, 70)
+  if (type == "age") {
+    ylim <- c(0, 25)
+  }
+  # set up panels
+  par(mfrow = c(1, 3), mar = c(2, 0, 2, 0), oma = c(2, 4, 1, 1))
+  for (f in fleets) {
+    plot(0,
+      xlim = c(1970, 2024),
+      ylim = ylim,
+      type = "n", xlab = "", ylab = "",
+      axes = FALSE
+    )
+    axis(1)
+    if (f == fleets[1]) {
+      axis(2, las = 1)
+    }
+    box()
+    SSMethod.TA1.8(mod_out, fleet = f, type = type, datonly = datonly, add = TRUE, set.pars = FALSE)
+    title(main = mod_out$FleetNames[f])
+  }
+  mtext(ifelse(type == "len", "Mean length (cm)", "Mean age (years)"),
+    outer = TRUE, side = 2, line = 2.5
+  )
+  mtext("Year", outer = TRUE, side = 1, line = 0.5)
+  dev.off()
+}
+# loop through fleet groups include fit or not to make plots
+for (fleet_group in 1:2) {
+  for (datonly in c(TRUE, FALSE)) {
+    for (type in c("len", "age")) {
+      if (fleet_group == 1) {
+        fleets <- 1:3
+      } else {
+        fleets <- 4:6
+      }
+      mean_length_multifleet(
+        fleets = fleets,
+        type = type,
+        filename = paste0(
+          "mean_", type, "_fishery",
+          ifelse(fleet_group == 1, "fisheries", "surveys"),
+          ifelse(datonly, "_data", "_fit"),
+          ".png"
+        ),
+        datonly = datonly
+      )
+    }
+  }
+}
